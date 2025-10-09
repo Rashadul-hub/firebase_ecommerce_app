dabd7ba9b206f41cb830506dc746a620c5c2ab9e
devhimuBlockVerse <himu.dev@blockversesolutions.com>
2025-10-09 12:57:44 +0600

    _CheckUserState Route Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ecommerce_flutter/controllers/auth_service.dart';
 import 'package:firebase_ecommerce_flutter/views/admin_home.dart';
 import 'package:firebase_ecommerce_flutter/views/login.dart';
 import 'package:firebase_ecommerce_flutter/views/signup.dart';
@@ -27,12 +28,45 @@ class MyApp extends StatelessWidget {
       ),
       routes: {
         // "/": (context) => AdminHome(),
-        "/": (context) => LoginPage(),
+        "/": (context) => CheckUser(),
         "/login": (context) => LoginPage(),
         "/signup": (context) => SignupPage(),
         "/home": (context) => AdminHome()
       },
 
+    );
+  }
+}
+
+
+class CheckUser extends StatefulWidget {
+  const CheckUser({super.key});
+
+  @override
+  State<CheckUser> createState() => _CheckUserState();
+}
+
+class _CheckUserState extends State<CheckUser> {
+
+  @override
+  void initState() {
+    AuthService().isLoggedIn().then((value) {
+      if(value){
+        Navigator.pushReplacementNamed(context, "/home");
+      }else{
+        Navigator.pushReplacementNamed(context, "/login");
+
+      }
+    });
+    super.initState();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
     );
   }
 }
